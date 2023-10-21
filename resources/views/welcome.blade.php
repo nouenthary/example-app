54f2d82f0ca8f5cc0dce23fb60f23ab0f8ccd889
nouenthary <tharynouen22@gmail.com>
2023-10-21 20:28:21 +0700

    dd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,6 +24,8 @@
 
         <a href="{{ url('home') }}">Home</a>   
 
+        <a href="{{ url('data') }}">Data</a>   
+
 
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
